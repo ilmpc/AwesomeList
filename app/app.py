b40ea6737c9473a2c51d006297d2fb862a70ad90
Mark Chigrin <dev@chigrin.ml>
2021-04-09 19:55:45 +0000

    Adding cache

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+import datetime as dt
+import copy
+import json
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import datetime as dt
-import copy
+from fastapi_utils.tasks import repeat_every
+import aiofiles
 import httpx
 
 from .services import get_data
@@ -12,8 +17,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-data = [{"name": "ilmpc/Algorithms", "stars": 100, "push_date": dt.datetime(2020, 11, 29)}]
-
+data = []
 
 @app.get("/alive")
 async def hi():
@@ -22,12 +26,29 @@ async def hi():
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request, star_ascending: bool = True):
-    now = dt.datetime.now()
+    now = dt.datetime.now(dt.timezone.utc)
     prepared_data = []
-    for repo in await get_data():
+    for repo in data:
         copied_repo = copy.copy(repo)
         copied_repo.days_last_push = (now - repo.last_push).days
         prepared_data.append(copied_repo)
     return templates.TemplateResponse(
         "index.j2", {"request": request, "repos": prepared_data}
     )
+
+@app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 12) #12h
+async def update_cache():
+    global data
+    now = dt.datetime.timestamp()
+    if Path(settings.cache_file).is_file():
+        async with aiofiles.open(settings.cache_file) as cache:
+            cache_data = json.load(cache)
+            if now - cache_data[timestamp] < 60 * 60 * 12:
+                data = cache_data[data]
+                return
+    
+    data = await get_data()
+    async with aiofiles.open(settings.cache_file, mode="w") as cache:
+        cache_data = {"timestamp": now, "data": data}
+        json.dump(cache_data, cache)
--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     debug: bool = False
     token: str = ""
-
+    cache_file: str = "repos.json"
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
